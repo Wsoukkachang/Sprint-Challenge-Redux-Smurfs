6f45036f9bdcb4c34f93a2274f67bae1541bd006
William Soukkachang <WSoukkachang@yahoo.com>
2019-05-04 11:42:54 -0500

    Intial commit. Set up Store and Actions. Setup reducer with fetching. Working on displaying fetched data.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {getSmurfs} from '../actions';
+
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,6 +10,11 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs;
+    console.log(this.props.smurfs);
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,4 +27,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+
+export default connect(mapStateToProps, {getSmurfs})(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,20 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAILURE,POST_START, POST_SUCCESS, POST_FAILURE} from '../actions';
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
 /*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ Your initial/default state for this project could *Although does not have to* look a lot like this*/
+ const intialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
- }
-*/
+  }
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +23,34 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_START: {
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: null
+      }
+    }
+
+    case FETCH_SUCCESS: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      }
+    }
+
+    case FETCH_FAILURE: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      }
+    }
+
+    default:
+      return state;
+  }
+}
